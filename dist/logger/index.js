611f9ed1373c6be4b38e029e5485c5166a22769b
SPANIER Nathan <spaniernathan67@gmail.com>
2021-11-05 01:45:00 +0100

    heroku

--- dist/logger/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const pino_1 = __importDefault(require("pino"));
-const logger = (0, pino_1.default)({});
-exports.default = logger;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvbG9nZ2VyL2luZGV4LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsZ0RBQXdCO0FBRXhCLE1BQU0sTUFBTSxHQUFHLElBQUEsY0FBSSxFQUFDLEVBQUUsQ0FBQyxDQUFDO0FBRXhCLGtCQUFlLE1BQU0sQ0FBQyJ9
